d8dec1193ef02a6544d5c3906085cbf40bb88501
dott12 <ives.bernal12@gmail.com>
2019-06-12 12:13:06 -0500

    primer intent

--- app/src/main/java/com/example/grammarhelp/MainActivity.java
+++ app/src/main/java/com/example/grammarhelp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.grammarhelp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toHomofonas (View view) {
+        Intent intent = new Intent(this, homofonasList.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/grammarhelp/homofonasList.java
@@ -0,0 +1,18 @@
+package com.example.grammarhelp;
+import android.widget.*;
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class homofonasList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homofonas_list);
+        Intent intent = getIntent();
+    }
+
+    
+}
