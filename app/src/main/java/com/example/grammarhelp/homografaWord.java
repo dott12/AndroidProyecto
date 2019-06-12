05ae7b65d57adf0430eade43ecde6ded0b44c539
dott12 <ives.bernal12@gmail.com>
2019-06-12 17:29:51 -0500

    solo 1 homografa

---
+++ app/src/main/java/com/example/grammarhelp/homografaWord.java
@@ -0,0 +1,13 @@
+package com.example.grammarhelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class homografaWord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homografa_word);
+    }
+}
